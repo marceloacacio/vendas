6dc9f45cde9fc76ab3d87c5db47a5f471dc96043
****3651 <marceloacacio26@gmail.com>
2020-03-25 10:45:38 -0300

    aula 8

---
+++ src/main/java/io/github/marceloacacio/repository/MyRepository.java
@@ -0,0 +1,7 @@
+package io.github.marceloacacio.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MyRepository {
+}
---
+++ src/main/java/io/github/marceloacacio/service/MyService.java
@@ -0,0 +1,7 @@
+package io.github.marceloacacio.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyService {
+}
