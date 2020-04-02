c98c07d4c4d3e8bf1ebaa710d091bc07f93b2c6a
****3651 <marceloacacio26@gmail.com>
2020-04-02 16:15:44 -0300

    aula 11

--- src/main/java/io/github/marceloacacio/MyConfiguration.java
+++ src/main/java/io/github/marceloacacio/MyConfiguration.java
@@ -1,10 +1,20 @@
 package io.github.marceloacacio;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("development")
 public class MyConfiguration {
+
+    @Bean
+    public CommandLineRunner executar(){
+        return args -> {
+            System.out.println("Teste");
+        };
+    }
 
     @Bean("applicationName")
     public String applicationName(){
